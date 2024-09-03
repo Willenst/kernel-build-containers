4a67a3f8addbbd4b9f36f847fbf7f7e8ed04d16f
Willenst <willenst@gmail.com>
2024-09-03 17:16:10 +0200

    proto_3

--- manage_containers.py
+++ manage_containers.py
@@ -1,193 +1,202 @@
+#!/usr/bin/python3
+
+"""
+This module provides functionality to manage Docker containers
+that are configured for building kernels with specific versions
+of GCC and Clang compilers.
+"""
+
 import os
 import subprocess
-import argparse
 import sys
-import re
-#раскраска вывода
-GREEN_COLOR = '\x1b[32m'
-RED_COLOR = '\x1b[31m'
-COLOR_END = '\x1b[0m'
-#закладка данных по контейнеры
-gcc_containers = {
-    'gcc-4.9' : 'ubuntu-16.04',
-    'gcc-5' : 'ubuntu-16.04',
-    'gcc-6' : 'ubuntu-18.04',
-    'gcc-7' : 'ubuntu-18.04',
-    'gcc-8' : 'ubuntu-20.04',
-    'gcc-9' : 'ubuntu-20.04',
-    'gcc-10' : 'ubuntu-20.04',
-    'gcc-11' : 'ubuntu-22.04',
-    'gcc-12' : 'ubuntu-22.04',
-    'gcc-13' : 'ubuntu-23.04',
-    'gcc-14' : 'ubuntu-24.04'
-}
-#таблица совместимости
-clang_gcc_compat = {
-    'clang-6' : ['gcc-8','gcc-9','gcc-10'], #20.04
-    'clang-7' : ['gcc-8','gcc-9','gcc-10'], #20.04
-    'clang-8' : ['gcc-8','gcc-9','gcc-10'], #20.04
-    'clang-9' : ['gcc-8','gcc-9','gcc-10'], #20.04
-    'clang-10' : ['gcc-8','gcc-9','gcc-10'], #20.04
-    'clang-11' : ['gcc-9','gcc-10','gcc-11','gcc-12'], #22.04
-    'clang-12' : ['gcc-8','gcc-9','gcc-10','gcc-11','gcc-12'], #20.04, 22.04
-    'clang-13' : ['gcc-9','gcc-10','gcc-11','gcc-12','gcc-13'], #22.04 23.04
-    'clang-14' : ['gcc-9','gcc-10','gcc-11','gcc-12','gcc-13'], #22.04 23.04 24.04
-    'clang-15' : ['gcc-9','gcc-10','gcc-11','gcc-12','gcc-13'], #22.04 23.04 24.04
-    'clang-16' : ['gcc-13','gcc-14'], #23.04 24.04
-    'clang-17' : ['gcc-13','gcc-14'], #23.04 24.04
-    'clang-18' : ['gcc-14'] #24.04
-}
-#проверка группы, решил закинуть в отдельную функцию
-def check_group():
-    result = subprocess.run(['groups'], capture_output=True, text=True)
-    if 'docker' in result.stdout:
-        return ''
-    else:
-        print("Hey, we gonna use sudo for running docker")
-        return 'sudo'
-#парсер, его немного переделал
-def createParser():
-    containers = list(gcc_containers.keys()) + list(clang_gcc_compat.keys())
-    containers += ['All']
-    parser = argparse.ArgumentParser()
-#валидатор входных данных, принимает clang-X, gcc-X, clang-X+gcc-Y, gcc-Y+clang-X
-#обработки неверных входных данных как таковой пока что нет, но она в планах!
-    def validate_input(value):
-        pattern = re.compile(r'(clang-\d+\+gcc-\d+)|(gcc-\d+\+clang-\d+)|(clang-\d+)|(gcc-\d+)')
-        if not pattern.match(value):
-            raise argparse.ArgumentTypeError(f"Invalid value '{value}'.")
-        return value
-#Тут отдельные аргументы под постройку и удаление контейнеров
-    parser.add_argument ('-a', '--add',
-                         type=validate_input,
-                         nargs='+',
-                         required=False,
-                         help='Add container with compiler \'gcc-X\' or '
-                              '\'clang-Y+gcc-X\' (or \'clang-X\' to autochoose gcc),'
-                              'write \'all\' to build all at once')
-    parser.add_argument ('-r', '--remove',
-                        type=validate_input,
-                        nargs='+',
-                        required=False,
-                        help='Remove container with compiler \'gcc-X\' or '
-                             '\'clang-Y+gcc-X\' (or \'clang-X\' to autochoose gcc),'
-                             'write \'all\' to remove all at once')
-    parser.add_argument ('-l','--list', action='store_true', help='Show supported compilers')
-    return parser
+import argparse
 
-#постройка контейнеров, практически не менялась
-def build_container(gcc_version,clang_version):
-    ubuntu_version=gcc_containers[f'gcc-{gcc_version}'].split('-')[1]
-    build_args=['--build-arg', f'GCC_VERSION={gcc_version}',
-                '--build-arg', f'UBUNTU_VERSION={ubuntu_version}',
-                '--build-arg', f'UNAME={os.getlogin()}',
-                '--build-arg', f'UID={os.getuid()}',
-                '--build-arg', f'GID={os.getgid()}'
-    ]
-    if clang_version:
-        print(f"\nbuilding_clang_container with clang-{clang_version} gcc-{gcc_version} ubuntu-{ubuntu_version}")
-        subprocess.run([SUDO_CMD, 'docker', 'build', '--build-arg', f'CLANG_VERSION={clang_version}',
-                         *build_args, '-t', f'kernel-build-container:clang-{clang_version}+gcc-{gcc_version}', '.'], text=True)
-    else:
-        print(f"\nbuilding_gcc_container with gcc-{gcc_version} ubuntu-{ubuntu_version}")
-        subprocess.run([SUDO_CMD, 'docker', 'build', *build_args, '-t', f'kernel-build-container:gcc-{gcc_version}', '.'], text=True)
+class Container:
+    """
+    Represents a Docker container configured for kernel building.
 
-def remove_container(container_name):
-    subprocess.run([SUDO_CMD, 'docker', 'rmi', f'kernel-build-container:{container_name}'], text=True)
-    print(f'\nRemoved container {container_name}\n')
-#функция под захват всех сделанных контейнеров из системы
+    Attributes:
+        gcc (str): Version of GCC compiler.
+        clang (str): Version of Clang compiler (optional).
+        ubuntu (str): Version of Ubuntu.
+        tag (str): Docker image tag.
+        is_alive (bool): Whether the container is built.
+    """
+    def __init__(self, gcc_version, clang_version, ubuntu_version):
+        self.gcc = gcc_version
+        self.clang = clang_version
+        self.ubuntu = ubuntu_version
+        self.tag = None
+        self.is_alive = self.live_check()
+
+    def add(self):
+        """Builds the Docker container with the specified compiler"""
+        build_args=['--build-arg', f'GCC_VERSION={self.gcc}',
+                    '--build-arg', f'UBUNTU_VERSION={self.ubuntu}',
+                    '--build-arg', f'UNAME={os.getlogin()}',
+                    '--build-arg', f'UID={os.getuid()}',
+                    '--build-arg', f'GID={os.getgid()}'
+        ]
+        if self.clang:
+            subprocess.run([SUDO_CMD, 'docker', 'build', '--build-arg',
+                            f'CLANG_VERSION={self.clang}', *build_args,
+                            '-t', f'kernel-build-container:clang-{self.clang}_gcc-{self.gcc}', '.'],
+                           text=True, check=True)
+            self.live_check()
+        else:
+            subprocess.run([SUDO_CMD, 'docker', 'build', *build_args,
+                            '-t', f'kernel-build-container:gcc-{self.gcc}', '.'],
+                           text=True, check=True)
+            self.live_check()
+
+    def rm(self):
+        """Removes the Docker container if it exists"""
+        if self.tag:
+            subprocess.run([SUDO_CMD, 'docker', 'rmi',
+                            f'kernel-build-container:{self.tag}'],
+                            text=True, check=True)
+            self.live_check()
+        else:
+            print('No such container')
+
+    def live_check(self):
+        """Checks if the Docker container with the specified tag exists"""
+        if self.gcc:
+            compiler = f'gcc-{self.gcc}'
+            cmd = subprocess.run([SUDO_CMD, 'docker', 'images', '--format', '{{.Tag}}'],
+                                 stdout=subprocess.PIPE,
+                                 text=True, check=True)
+            container_tags=cmd.stdout.strip().split()
+            for tag in container_tags:
+                if compiler in tag:
+                    self.tag = tag
+                    return True
+        return False
+
 def extract_containers():
-    containers = subprocess.run([SUDO_CMD, 'docker', 'images', '--format', '{{.Tag}}'], 
-                        stdout=subprocess.PIPE, 
-                        text=True)
+    """Extracts the list of existing container tags from Docker"""
+    containers = subprocess.run([SUDO_CMD, 'docker', 'images', '--format', '{{.Tag}}'],
+                                stdout=subprocess.PIPE,
+                                text=True, check=True)
     compilers=containers.stdout.strip().split()
     return compilers
-#функция под вывод контейнеров, это очень далеко не ее финальный вид, пока что это просто затычка
-#сейчас я думаю сделать столбик контейнеров gcc
-#затем столбик контейнеров clang, у них будут отдельно выведены совместимые gcc
-#затем столбик собранных контейнеров
-#если gcc или clang где то используются (неважно, отдельно, вместе), они будут гореть зеленым и будут помечены "+"
-#также их будет само собой отображать в сбилженном контейнере в столбике контейнеров
-def list_containers(): 
-    all_containers = list(gcc_containers.keys())
-    current_containers = extract_containers()
-    for el in all_containers:
-        if el in current_containers:
-            color=GREEN_COLOR
-            print(color+'[+] '+el+COLOR_END)
-        else:
-            color=RED_COLOR
-            print(color+'[-] '+el+COLOR_END)
-#Парсинг версии gcc/clang, вход не важен, будет работать как для их комбинации такк и под отдельные
-def parse_versions(text):
-    #actualy need to refactor this
-    clang_version = re.findall(r'clang-(\d+)', text)
-    gcc_version = re.findall(r'gcc-(\d+)', text)
-    if clang_version:
-        clang = clang_version[0]
-    else: 
-        clang = None
-    if gcc_version:
-        gcc = gcc_version[0]
-    else: 
-        gcc = None
-    return clang, gcc
-#проверка совместимости, будет оповещать если юзер выбрал неподходящие версии gcc и clang
-def comtability_check(gcc_version,clang_version):
-    available_gcc_versions = clang_gcc_compat[f'clang-{clang_version}']
-    gcc = f'gcc-{gcc_version}'
-    if gcc in available_gcc_versions:
-        return True
-    else:
-        print(f'Incompatible gcc and clang, for clang-{clang_version}, choosee gcc from ' + ', '.join(available_gcc_versions))
-        sys.exit()
-#обработчик вхорда, работает под комбинированный и отдельный вход
-def add_handler(add):
-    for compiler in add:
-        if compiler in extract_containers():
-            print(compiler+' already added')
-            continue
-        if compiler == 'all':
-            print('not implemented yet!') #сборку всех сразу сделаю позже
-            sys.exit()
 
-        gcc_version, clang_version = parse_versions(compiler)
+def check_group():
+    """Checks if the user is in the Docker group, returns 'sudo' if not"""
+    result = subprocess.run(['groups'], capture_output=True,
+                            text=True, check=True)
+    if 'docker' in result.stdout:
+        return ''
+    print("Hey, we gonna use sudo for running docker")
+    return 'sudo'
 
-        if clang_version and gcc_version:
-            if comtability_check(gcc_version,clang_version):
-                build_container(gcc_version,clang_version)
+def add_handler(needed_compiler,containers):
+    """Adds the specified container(s) based on the provided compiler"""
+    if needed_compiler == 'all':
+        for c in containers:
+            print(f'Adding container for {needed_compiler} '
+                    'on {c.ubuntu} with gcc {c.gcc} and clang {c.clang}')
+            c.add()
+        sys.exit(0)
+    for c in containers:
+        if 'gcc-' + c.gcc == needed_compiler:
+            print(f'Adding container for {needed_compiler} '
+                    'on {c.ubuntu} with gcc {c.gcc} and clang {c.clang}')
+            c.add()
+            sys.exit(0)
+        if c.clang and 'clang-' + c.clang == needed_compiler:
+            print(f'Adding container for {needed_compiler} '
+                    'on {c.ubuntu} with gcc {c.gcc} and clang {c.clang}')
+            c.add()
+            sys.exit(0)
+    sys.exit('[!] ERROR: no container with the compiler "{needed_compiler}"')
 
-        elif clang_version: #automated gcc chose
-            gcc_version = clang_gcc_compat[f'clang-{clang_version}']
-            build_container(gcc_version[-1],clang_version)
+def remove_handler(removed_compiler,containers):
+    """Removes the specified container(s) based on the provided compiler"""
+    if removed_compiler == 'all':
+        for c in containers:
+            print(f'Removing container for {removed_compiler} '
+                    'on {c.ubuntu} with gcc {c.gcc} and clang {c.clang}')
+            c.rm()
+        sys.exit(0)
+    for c in containers:
+        if 'gcc-' + c.gcc == removed_compiler:
+            print(f'Removing container for {removed_compiler} '
+                    'on {c.ubuntu} with gcc {c.gcc} and clang {c.clang}')
+            c.rm()
+            sys.exit(0)
+        if c.clang and 'clang-' + c.clang == removed_compiler:
+            print(f'Removing container for {removed_compiler} '
+                    'on {c.ubuntu} with gcc {c.gcc} and clang {c.clang}')
+            c.rm()
+            sys.exit(0)
 
-        elif gcc_version:
-            build_container(gcc_version,None)
-#обработчик удаления, должен работать по тегу контейнера
-def remove_handler(remove):
-    for compiler in remove:
-        if compiler == 'all':
-            print('not implemented yet!') #удаление всех сразу сделаю позже
-            sys.exit()
-        if compiler in extract_containers():
-            remove_container(compiler)
-        else:
-            print('No such container :',compiler)
+def main():
+    """Main function to manage the containers"""
+    containers = []
+    containers += [Container("4.9", None, "16.04")]
+    containers += [Container("5", None, "16.04")]
+    containers += [Container("6", None, "18.04")]
+    containers += [Container("7", None, "18.04")]
+    containers += [Container("8", None, "20.04")]
+    containers += [Container("9", None, "20.04")]
+    containers += [Container("10", None, "20.04")]
+    containers += [Container("11", "12", "22.04")]
+    containers += [Container("12", "13", "22.04")]
+    containers += [Container("12", "14", "22.04")]
+    containers += [Container("13", "15", "23.04")]
+    containers += [Container("14", "16", "24.04")]
+    containers += [Container("14", "17", "24.04")]
 
+    compilers = ['gcc-4.9',
+                 'gcc-5',
+                 'gcc-6',
+                 'gcc-7',
+                 'gcc-8',
+                 'gcc-9',
+                 'gcc-10',
+                 'gcc-11',
+                 'gcc-12',
+                 'gcc-13',
+                 'gcc-14',
+                 'clang-12',
+                 'clang-13',
+                 'clang-14',
+                 'clang-15',
+                 'clang-16',
+                 'clang-17',
+                 'all']
 
-if __name__ == '__main__':
-
-    parser = createParser()
+    parser = argparse.ArgumentParser(description='Manage the kernel-build-containers')
+    parser.add_argument('-l','--list', action='store_true',
+                        help='show the kernel build containers')
+    parser.add_argument('-a','--add', choices=compilers, nargs='+',
+                        help='build a container with this compiler (\'all\' to build all of them)')
+    parser.add_argument('-r','--remove', choices=compilers, nargs='+',
+                    help='remove a container with this compiler (\'all\' to remove all of them)')
     args = parser.parse_args()
-    SUDO_CMD = check_group()
-    add = args.add
-    remove = args.remove
-    if args.list:
-        print('DEMO! IN DEVELOPMENT!')
-        list_containers()
-        sys.exit()
-    if add:
-        add_handler(add)
-    if remove:
-        remove_handler(remove)
 
+    if args.list:
+        for c in containers:
+            if c.is_alive:
+                status = '[+]'
+            else:
+                status = '[-]'
+            print(f'container with gcc {c.gcc} and clang {c.clang} on ubuntu {c.ubuntu}: {status}')
+        sys.exit(0)
+
+    if args.add:
+        for compiler in args.add:
+            add_handler(compiler,containers)
+
+
+    if args.remove:
+        for compiler in args.remove:
+            remove_handler(compiler,containers)
+
+    parser.print_help()
+    sys.exit(0)
+
+SUDO_CMD = check_group()
+if __name__ == '__main__':
+    main()
