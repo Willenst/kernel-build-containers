0efa2d2f402ca39149f4066f7ee6d3298090d3df
Willenst <willenst@gmail.com>
2024-09-11 20:10:25 +0200

    typo fixes

--- manage_containers.py
+++ manage_containers.py
@@ -56,7 +56,7 @@ class Container:
 
     def add(self):
         """Builds the Docker container with the specified compiler"""
-        build_args=['--build-arg', f'GCC_VERSION={self.gcc}',
+        build_args = ['--build-arg', f'GCC_VERSION={self.gcc}',
                     '--build-arg', f'UBUNTU_VERSION={self.ubuntu}',
                     '--build-arg', f'UNAME={os.getlogin()}',
                     '--build-arg', f'UID={os.getuid()}',
@@ -64,8 +64,8 @@ class Container:
                     '-t', f'kernel-build-container:gcc-{self.gcc}'
         ]
         if self.clang:
-            build_args=['--build-arg', f'CLANG_VERSION={self.clang}']+build_args+ \
-                       ['-t', f'kernel-build-container:clang-{self.clang}']
+            build_args = ['--build-arg', f'CLANG_VERSION={self.clang}']+build_args+ \
+                         ['-t', f'kernel-build-container:clang-{self.clang}']
         if self.quiet_option:
             build_args = build_args + ['-q']
         subprocess.run([self.sudo, 'docker', 'build', *build_args, '.'],
@@ -86,14 +86,14 @@ class Container:
         cmd = subprocess.run([self.sudo, 'docker', 'images', *search, '--format', '{{.ID}}'],
                                 stdout=subprocess.PIPE,
                                 text=True, check=True)
-        container_id=cmd.stdout.strip()
+        container_id = cmd.stdout.strip()
         self.id = container_id
         return container_id
 
 def check_group():
     """Checks if the user is in the Docker group, returns 'sudo' if not"""
-    result = subprocess.run(['groups'], capture_output=True,
-                            text=True, check=True)
+    result = subprocess.run(['groups'], capture_output = True,
+                            text = True, check = True)
     if 'docker' in result.stdout or 'root' in result.stdout:
         return ''
     print('We need to use sudo for running docker')
@@ -147,14 +147,14 @@ def list_containers(containers):
 def main():
     """Main function to manage the containers"""
     parser = argparse.ArgumentParser(description='Manage the kernel-build-containers')
-    parser.add_argument('-l','--list', action='store_true',
-                        help='show the kernel build containers')
-    parser.add_argument('-a', '--add', choices=compilers, metavar='compiler',
+    parser.add_argument('-l','--list', action = 'store_true',
+                        help = 'show the kernel build containers')
+    parser.add_argument('-a', '--add', choices = compilers, metavar = 'compiler',
                         help=f'build a container with this compiler: ({", ".join(compilers)}, where "all" for all of the compilers)')
-    parser.add_argument('-r', '--remove', choices=['all'], metavar='all',
-                        help='remove all created containers')
-    parser.add_argument('-q','--quiet', action='store_true',
-                    help='Suppress the build output and print image ID on success')
+    parser.add_argument('-r', '--remove', choices = ['all'], metavar = 'all',
+                        help = 'remove all created containers')
+    parser.add_argument('-q','--quiet', action = 'store_true',
+                        help = 'Suppress the build output and print image ID on success')
     args = parser.parse_args()
 
     if not any([args.list, args.add, args.remove, args.quiet]):
